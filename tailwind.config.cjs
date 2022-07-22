1aba0263a0b4a33d102f1b1c9166a5293c5e7fb0
NatsukoSeki <seki72ko@gmail.com>
2022-07-23 07:03:53 +0900

    add:Challenges for tailwind css.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     './src/**/*.{html,js}', // here
   ],
   theme: {
-    extend: {},
-    screens: {
-      tablet: '750px',
-      pc: '960px',
+    extend: {
+      colors: {
+        baseColor: '#09558c',
+      },
+      screens: {
+        tablet: '750px',
+        pc: '960px',
+      },
     },
   },
   plugins: [],
